51a8ebc75e8740332b9a1494b8b45132f20b6dda
Antwhale <olollolo339@naver.com>
2025-05-04 18:49:27 +0900

    Initial Commit

---
+++ SaleWar/SaleWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaleWarApp.swift
+//  SaleWar
+//
+//  Created by 부재식 on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SaleWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
